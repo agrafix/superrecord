15bb75e0b9fe3e494a365f428f5c6ab3c92c60bb
Alexander Thiemann <mail@athiemann.net>
2017-06-29 18:07:08 +0200

    add setter and some docs + sugar

--- src/SuperRecord.hs
+++ src/SuperRecord.hs
@@ -8,18 +8,44 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-module SuperRecord where
+{-# LANGUAGE RankNTypes #-}
+module SuperRecord
+    ( (:=)(..)
+    , Rec, rnil, rcons, (&)
+    , get, set
+    , RecTyIdxH, RecIdxTyH
+    )
+where
 
 import Data.Dynamic
+import GHC.Exts
 import GHC.OverloadedLabels
 import GHC.TypeLits
 import qualified Data.Vector as V
+import qualified Data.Vector.Mutable as VM
 
+-- | Field named @l@ labels value of type @t@ adapted from the awesome /labels/ package.
+-- Example: @(#name := \"Chris\") :: (\"name\" := String)@
 data label := value = KnownSymbol label => FldProxy label := value
 deriving instance Typeable (:=)
 deriving instance Typeable (label := value)
 infix 6 :=
 
+instance (Eq value) => Eq (label := value) where
+  (_ := x) == (_ := y) = x == y
+  {-# INLINE (==) #-}
+
+instance (Ord value) => Ord (label := value) where
+  compare (_ := x) (_ := y) = x `compare` y
+  {-# INLINE compare #-}
+
+instance (Show t) =>
+         Show (l := t) where
+  showsPrec p (l := t) =
+    showParen (p > 10) (showString ("#" ++ (symbolVal l) ++ " := " ++ show t))
+
+-- | A proxy witness for a label. Very similar to 'Proxy', but needed to implement
+-- a non-orphan 'IsLabel' instance
 data FldProxy (t :: Symbol)
     = FldProxy
     deriving (Show, Read, Eq, Ord, Typeable)
@@ -27,15 +53,26 @@ data FldProxy (t :: Symbol)
 instance l ~ l' => IsLabel (l :: Symbol) (FldProxy l') where
     fromLabel _ = FldProxy
 
+-- | The core record type.
 newtype Rec (lts :: [*])
    = Rec { unRec :: V.Vector Dynamic }
 
+-- | An empty record
 rnil :: Rec '[]
 rnil = Rec V.empty
+{-# INLINEABLE rnil #-}
 
+-- | Prepend a record entry to a record 'Rec'
 rcons :: Typeable t => l := t -> Rec lts -> Rec (l := t ': lts)
 rcons (_ := val) (Rec vec) =
     Rec $ V.cons (toDyn val) vec
+{-# INLINEABLE rcons #-}
+
+-- | Alias for 'rcons'
+(&) :: Typeable t => l := t -> Rec lts -> Rec (l := t ': lts)
+(&) = rcons
+
+infixr 5 &
 
 type family RecTyIdxH (i :: Nat) (l :: Symbol) (lts :: [*]) :: Nat where
     RecTyIdxH idx l (l := t ': lts) = idx
@@ -61,7 +98,21 @@ get ::
     ) => FldProxy l -> Rec lts -> v
 get _ (Rec vec) =
     let readAt = fromIntegral $ natVal (Proxy :: Proxy idx)
-        val = vec V.! readAt
+        val = V.unsafeIndex vec readAt
     in case fromDynamic val of
          Nothing -> error "SuperRecord: internal type error. This should not happen"
          Just v -> v
+{-# INLINEABLE get #-}
+
+set ::
+    forall l v lts idx.
+    ( RecTyIdxH 0 l lts ~ idx
+    , RecIdxTyH idx 0 lts ~ v
+    , KnownNat idx
+    , Typeable v
+    ) => FldProxy l -> v -> Rec lts -> Rec lts
+set _ val r =
+    let setAt = fromIntegral $ natVal (Proxy :: Proxy idx)
+        dynVal = toDyn val
+    in Rec (V.modify (\v -> VM.unsafeWrite v setAt dynVal) $ unRec r)
+{-# INLINEABLE set #-}
--- test/Spec.hs
+++ test/Spec.hs
@@ -8,10 +8,17 @@ import Test.Hspec
 type Ex1 = '["foo" := String, "int" := Int]
 
 r1 :: Rec Ex1
-r1 = rcons (#foo := "Hi") $ rcons (#int := 213) rnil
+r1 =
+    #foo := "Hi"
+    & #int := 213
+    & rnil
 
 main :: IO ()
 main = hspec $
     do it "getter works" $
            do get #foo r1 `shouldBe` "Hi"
               get #int r1 `shouldBe` 213
+       it "setter works" $
+           do let r2 = set #foo "Hey" r1
+              get #foo r1 `shouldBe` "Hi"
+              get #foo r2 `shouldBe` "Hey"
